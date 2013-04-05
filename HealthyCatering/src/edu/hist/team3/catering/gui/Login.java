7287df305bb42aadf628fe9e23a771d31753da48
NeederLaptop <NeederLaptop@158.38.190.41>
2013-04-05 12:23:21 +0200

    Started work on Login.java, added ClientStart.java. The main starting file of the program

--- HealthyCatering/src/edu/hist/team3/catering/gui/Login.java
+++ HealthyCatering/src/edu/hist/team3/catering/gui/Login.java
@@ -1,9 +1,18 @@
 package edu.hist.team3.catering.gui;
 
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
 public class Login {
 	
 	public Login() {
+		JFrame frame = new JFrame("User Login");
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		
+		frame.setSize(toolkit.getScreenSize());
+		frame.setUndecorated(true);
+		frame.setVisible(true);
 	}
 	
 }

a3f18e5d5b8798ce595457d7017c4cc24b7c32ba
Andreas <Andreas@129.241.166.42>
2013-04-05 12:24:09 +0200

    created MenuGUI with details for methods

---
+++ HealthyCatering/src/edu/hist/team3/catering/gui/MenuGUI.java
@@ -0,0 +1,23 @@
+package edu.hist.team3.catering.gui;
+/**
+ * 
+ *
+ *Menu GUI
+ *--
+ *--
+ *+ findResources()
+ *+ editResources()
+ *+ addResources()
+ *+ removeResource()
+ *+ findDishes()
+ *+ editDises()
+ *+ addDish()
+ *+ removeDish()
+ *+ findMenus()
+ *+ addMenu()
+ *+ editMenu()
+ *+ removeMenu()
+ */
+public class MenuGUI {
+
+}

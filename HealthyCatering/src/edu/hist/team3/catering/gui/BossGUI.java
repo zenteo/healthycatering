1b2ea632461005fd87de701c603b60e5b7ec5a66
Andreas <Andreas@129.241.166.42>
2013-04-05 14:26:00 +0200

    Editing the rest of the GUI's

--- HealthyCatering/src/edu/hist/team3/catering/gui/BossGUI.java
+++ HealthyCatering/src/edu/hist/team3/catering/gui/BossGUI.java
@@ -1,16 +1,57 @@
 package edu.hist.team3.catering.gui;
+
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /*
  * Boss GUI
---
---
-+ addEmployee()
-+ editEmployee()
-+ getEmployeeStats()
-+ removeEmployee()
-+ addJob()
-+ editJob()
-+ removeJob()
+ --
+ --
+ + addEmployee()
+ + editEmployee()
+ + getEmployeeStats()
+ + removeEmployee()
+ + addJob()
+ + editJob()
+ + removeJob()
  */
 public class BossGUI {
+
+	JFrame frame;
+	Toolkit toolkit;
+
+	public BossGUI() {
+		frame = new JFrame("Boss");
+		frame.setLayout(new GridLayout(4, 2));
+		toolkit = Toolkit.getDefaultToolkit();
+		frame.setSize(toolkit.getScreenSize());
+		frame.setUndecorated(true);
+
+		JButton exitButton = new JButton("Quit");
+		exitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				quitProgram();
+			}
+		});
+		frame.add(exitButton);
+
+		frame.setVisible(true);
+
+	}
+
+	private void quitProgram() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		WindowEvent closingEvent = new WindowEvent(frame,
+				WindowEvent.WINDOW_CLOSING);
+		toolkit.getDefaultToolkit().getSystemEventQueue()
+				.postEvent(closingEvent);
+	}
 
 }
--- HealthyCatering/src/edu/hist/team3/catering/gui/StocksGUI.java
+++ HealthyCatering/src/edu/hist/team3/catering/gui/StocksGUI.java
@@ -1,4 +1,12 @@
 package edu.hist.team3.catering.gui;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 /*
  * Stocks GUI
 --
@@ -10,5 +18,37 @@ package edu.hist.team3.catering.gui;
 
  */
 public class StocksGUI {
+
+	JFrame frame;
+	Toolkit toolkit;
+
+	public MenuGUI() {
+		frame = new JFrame("Stocks");
+		frame.setLayout(new GridLayout(3,2));
+		toolkit = Toolkit.getDefaultToolkit();
+		frame.setSize(toolkit.getScreenSize());
+		frame.setUndecorated(true);
+
+		JButton exitButton = new JButton("Quit");
+		exitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				quitProgram();
+			}
+		});
+		frame.add(exitButton);
+
+		frame.setVisible(true);
+
+	}
+
+	private void quitProgram() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		WindowEvent closingEvent = new WindowEvent(frame,
+				WindowEvent.WINDOW_CLOSING);
+		toolkit.getDefaultToolkit().getSystemEventQueue()
+				.postEvent(closingEvent);
+	}
+
 
 }

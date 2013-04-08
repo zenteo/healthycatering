d25b802a9a85f9983f8a8c2589a1366f548b5c8d
NeederLaptop <NeederLaptop@158.38.190.41>
2013-04-08 16:41:20 +0200

    did nothing

--- HealthyCatering/src/edu/hist/team3/catering/gui/MainGUI.java
+++ HealthyCatering/src/edu/hist/team3/catering/gui/MainGUI.java
@@ -10,7 +10,7 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
-import javax.swing.JMenuBar;
+import javax.swing.JMenuBar; 
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;

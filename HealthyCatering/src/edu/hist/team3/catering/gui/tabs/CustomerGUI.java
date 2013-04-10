0fd02e75757974bc4585fab860ac6fa858bbd68b
NeederLaptop <NeederLaptop@158.38.190.41>
2013-04-10 13:54:09 +0200

    Working on customer gui

--- HealthyCatering/src/edu/hist/team3/catering/gui/tabs/CustomerGUI.java
+++ HealthyCatering/src/edu/hist/team3/catering/gui/tabs/CustomerGUI.java
@@ -1,12 +1,15 @@
 package edu.hist.team3.catering.gui.tabs;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 import edu.hist.team3.catering.database.Customer;
 import edu.hist.team3.catering.database.managers.CustomerManager;
@@ -26,24 +29,32 @@ import edu.hist.team3.catering.database.managers.CustomerManager;
  */
 @SuppressWarnings("serial")
 public class CustomerGUI extends JPanel {
-	JScrollPane customerScrollPane;
-	JList<Customer> customerList;
-	CustomerManager cManager;
+	private JScrollPane customerScrollPane;
+	private JList<Customer> customerList;
+	private CustomerManager cManager;
+	private JTextField searchBar;
 	
 	public CustomerGUI() {
-		setLayout(new FlowLayout());
+		setLayout(new BorderLayout());
 		
 		cManager = CustomerManager.getInstance();
 		
 		Customer[] list = cManager.getCustomers();
 		System.out.println(list[0]);
 		customerList = new JList<Customer>(list);
-		
+
+		JPanel leftPanel = new JPanel();
+		leftPanel.setPreferredSize(new Dimension(700, 640));
+		leftPanel.setLayout(new FlowLayout());
 		customerScrollPane = new JScrollPane(customerList);
 		customerScrollPane.setPreferredSize(new Dimension(500, 600));
+		searchBar = new JTextField("Search bar");
+		searchBar.selectAll();
+		searchBar.setPreferredSize(new Dimension(300, 30));
+		leftPanel.add(searchBar);
+		leftPanel.add(customerScrollPane);
 		
-		this.add(customerScrollPane);
-		this.add(new JButton("Test"));
+		add(leftPanel, BorderLayout.WEST);
 	}
 
 }

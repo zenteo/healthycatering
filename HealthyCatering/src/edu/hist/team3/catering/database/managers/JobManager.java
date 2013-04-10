cbdd622649af9c629237efb9ec06438f2a476f3b
Andreas <Andreas@129.241.166.99>
2013-04-10 13:24:41 +0200

    updated jobmanager

--- HealthyCatering/src/edu/hist/team3/catering/database/managers/JobManager.java
+++ HealthyCatering/src/edu/hist/team3/catering/database/managers/JobManager.java
@@ -1,9 +1,6 @@
 package edu.hist.team3.catering.database.managers;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import edu.hist.team3.catering.database.Customer;
 import edu.hist.team3.catering.database.DatabaseManager;
 import edu.hist.team3.catering.database.Job;
 
@@ -25,29 +22,6 @@ public class JobManager {
 
 	private JobManager() {
 		manager = DatabaseManager.getInstance();
-	}
-
-	/**
-	 * Return (if found) an Job with inputted username and password
-	 * 
-	 * @param username
-	 * @param password
-	 * @return Job
-	 */
-	public Job JobManager(String username, String password) {
-		ResultSet result = manager
-				.performSQL("SELECT customer_id FROM Job, Customer WHERE username ='"
-						+ username + "' AND password ='" + password + "'");
-
-		try {
-			if (result.next()) {
-				return new Job(manager, new Customer(manager, result.getInt(1)));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return null;
 	}
 
 	/**

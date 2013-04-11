31414185bfd9e6217d86c0df1e6fd690ad44b87a
Andreas <Andreas@158.38.186.236>
2013-04-11 10:50:23 +0200

    cookingGUI updated

--- HealthyCatering/src/edu/hist/team3/catering/gui/tabs/CookingGUI.java
+++ HealthyCatering/src/edu/hist/team3/catering/gui/tabs/CookingGUI.java
@@ -1,5 +1,10 @@
 package edu.hist.team3.catering.gui.tabs;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /*
@@ -13,8 +18,46 @@ import javax.swing.JPanel;
  */
 @SuppressWarnings("serial")
 public class CookingGUI extends JPanel {
-	
+
 	public CookingGUI() {
-		
+		Dimension buttonDimension = new Dimension(150, 70);
+
+		JButton getDeliveriesButton = new JButton("Get Deliveries");
+		getDeliveriesButton.setPreferredSize(buttonDimension);
+		getDeliveriesButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+
+		});
+		JButton getDishesOfADeliveryButton = new JButton("Get dishes of a Deliveries");
+		getDishesOfADeliveryButton.setPreferredSize(buttonDimension);
+		getDishesOfADeliveryButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+
+		});
+		JButton getResourcesOfADishButton = new JButton("Get Resources of a dish");
+		getResourcesOfADishButton.setPreferredSize(buttonDimension);
+		getResourcesOfADishButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+
+		});
+		JButton changeDeliveryStatusButton = new JButton("Change delivery status");
+		changeDeliveryStatusButton.setPreferredSize(buttonDimension);
+		changeDeliveryStatusButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+
+		});
+
 	}
 }

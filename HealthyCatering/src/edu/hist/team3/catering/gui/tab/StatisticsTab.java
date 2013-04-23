48adda9a3fbbeeaf2a31a8c488c293c75198c094
NeederLaptop <NeederLaptop@158.38.185.212>
2013-04-23 12:24:34 +0200

    Added stuff for charts

--- HealthyCatering/src/edu/hist/team3/catering/gui/tab/StatisticsTab.java
+++ HealthyCatering/src/edu/hist/team3/catering/gui/tab/StatisticsTab.java
@@ -1,6 +1,14 @@
 package edu.hist.team3.catering.gui.tab;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
 
 import edu.hist.team3.catering.database.manager.Services;
 
@@ -9,5 +17,17 @@ public class StatisticsTab extends JPanel {
 	
 	public StatisticsTab(Services services) {
 		this.services = services;
+		setLayout(new BorderLayout());
+		displayTestPieChart();
+	}
+	
+	public void displayTestPieChart() {
+		DefaultPieDataset data = new DefaultPieDataset();
+		data.setValue("The pie!", 50);
+		data.setValue("Not the pie!", 50);
+		
+		JFreeChart chart = ChartFactory.createPieChart3D("Fucking pie", data, true, true, false);
+		ChartPanel frame = new ChartPanel(chart);
+		add(frame, BorderLayout.CENTER);
 	}
 }

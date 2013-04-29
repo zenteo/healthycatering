d4b5bf42bb176f486d5b5321784379f4bd7ad090
Andreas <Andreas@129.241.166.55>
2013-04-29 10:55:41 +0200

    JavaDoc customerplanlist

--- HealthyCatering/src/edu/hist/team3/catering/database/CustomerPlanList.java
+++ HealthyCatering/src/edu/hist/team3/catering/database/CustomerPlanList.java
@@ -9,35 +9,57 @@ import java.util.Iterator;
 public class CustomerPlanList {
 	private HashSet<Plan> plans = null;
 	private final Customer customer;
-	
+	/**
+	 * Creates a new instance of CustomerPlanList
+	 * 
+	 * @param customer
+	 */
 	public CustomerPlanList(Customer customer) {
 		assert(customer != null);
 		this.customer = customer;
 	}
-	
+	/**
+	 * Gives the Customer
+	 * @return Customer
+	 */
 	public Customer getCustomer() {
 		return customer;
 	}
-	
+	/**
+	 * Adds a plan
+	 * @param plan
+	 * @throws SQLException
+	 */
 	public void add(Plan plan) throws SQLException {
 		assert(plan != null);
 		plan.setCustomer(customer);
 		plan.commit();
 		plans.add(plan);
 	}
-	
+	/**
+	 * Add an empty plan
+	 * @return plan
+	 * @throws SQLException
+	 */
 	public Plan add() throws SQLException {
 		Plan ret = getCustomer().getManager().createPlan(getCustomer());
 		plans.add(ret);
 		return ret;
 	}
-	
+	/**
+	 * Removes a plan
+	 * @param plan
+	 * @throws SQLException
+	 */
 	public void remove(Plan plan) throws SQLException {
 		assert(plan != null);
 		plan.remove();
 		plans.remove(plan);
 	}
-	
+	/**
+	 * Removes all plans
+	 * @throws SQLException
+	 */
 	public void removeAll() throws SQLException {
 		Iterator<Plan> it = iterator();
 		while (it.hasNext()) {
@@ -45,17 +67,27 @@ public class CustomerPlanList {
 		}
 		plans.clear();
 	}
-	
+	/**
+	 * Gives what a plan contains
+	 * @param plan
+	 * @return Contents of a plan
+	 */
 	public boolean contains(Plan plan) {
 		tryFetch();
 		return plans.contains(plan);
 	}
-	
+	/**
+	 * Gives a list of plans
+	 * @return list of plans
+	 */
 	public Iterator<Plan> iterator() {
 		tryFetch();
 		return plans.iterator();
 	}
-	
+	/**
+	 * Gives the number of plans
+	 * @return number of plans
+	 */
 	public int size() {
 		tryFetch();
 		return plans.size();

08943181435ce9884aeaa48976bcd6ff69097dd7
Andreas <Andreas@129.241.166.55>
2013-04-29 11:46:58 +0200

    Javadoc	Customer search frame

--- HealthyCatering/src/edu/hist/team3/catering/gui/frame/CustomerSearchFrame.java
+++ HealthyCatering/src/edu/hist/team3/catering/gui/frame/CustomerSearchFrame.java
@@ -15,7 +15,10 @@ import edu.hist.team3.catering.gui.panel.CustomerSearch;
 public class CustomerSearchFrame extends JFrame {
 	private CustomerSearch customerSearch;
 	private Customer selected;
-	
+	/**
+	 * Creates a new instance of CustomerSearchFrame based on services
+	 * @param services
+	 */
 	public CustomerSearchFrame(Services services) {
 
 		this.customerSearch = new CustomerSearch(services);
@@ -34,11 +37,17 @@ public class CustomerSearchFrame extends JFrame {
 		setTitle("Select customer");
 		setSize(300, 200);
 	}
-
+	/**
+	 * Gives selected Customer
+	 * @return customer
+	 */
 	public Customer getSelected() {
 		return selected;
 	}
-
+	/**
+	 * Sets selected Customer
+	 * @param selected
+	 */
 	public void setSelected(Customer selected) {
 		this.selected = selected;
 	}
